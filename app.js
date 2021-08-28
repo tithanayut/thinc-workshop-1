a90852785779041375a404cf403321b2a6facf85
Thanayut T <b@thanayut.in.th>
2021-08-28 17:02:58 +0700

    fix: js reference error

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // populate DOM
     const nf = new Intl.NumberFormat();
-    document.getElementById("cases-new") = "+" + nf.format(data.todayCases);
-    document.getElementById("cases-total") = nf.format(data.cases);
+    document.getElementById("cases-new").innerText =
+        "+" + nf.format(data.todayCases);
+    document.getElementById("cases-total").innerText = nf.format(data.cases);
     document.getElementById("cases-active").innerText = nf.format(data.active);
-    document.getElementById("cases-critical").innerText = nf.format(data.critical);
-    document.getElementById("deaths-new").innerText = "+" + nf.format(data.todayDeaths);
+    document.getElementById("cases-critical").innerText = nf.format(
+        data.critical
+    );
+    document.getElementById("deaths-new").innerText =
+        "+" + nf.format(data.todayDeaths);
     document.getElementById("deaths-total").innerText = nf.format(data.deaths);
-    document.getElementById("recovered-new").innerText = "+" + nf.format(data.todayRecovered);
-    document.getElementById("recovered-total").innerText = nf.format(data.recovered);
+    document.getElementById("recovered-new").innerText =
+        "+" + nf.format(data.todayRecovered);
+    document.getElementById("recovered-total").innerText = nf.format(
+        data.recovered
+    );
 });
